b925d81079f8ce42a7b2aab6c69f7b5b41d9412a
ilcors-dev <corsetti.luca28@gmail.com>
2022-07-14 22:37:47 +0200

    added: infos

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,45 @@
 <template>
-	<navbar class="flex justify-between items-center">
+	<nav class="flex flex-col items-center justify-between md:flex-row">
 		<MemojiLoop />
-		<div class="flex justify-around">
-			<div class="w-[200px]">
-				<p class="font-bold">Luca Corsetti</p>
-				<GlitchText class="cursor-pointer" text="fullstack developer" />
+		<div
+			class="mt-10 flex w-full flex-col sm:justify-between md:mt-0 md:flex-row md:justify-around"
+		>
+			<div
+				class="order-2 mt-10 flex flex-col space-y-5 md:order-1 md:mt-0 md:flex-row md:space-y-0 lg:ml-auto lg:mr-28"
+			>
+				<div class="w-[200px] md:w-[150px] lg:w-[200px]">
+					<p class="font-bold tracking-tight">member</p>
+					<GlitchText
+						class="cursor-pointer tracking-tighter"
+						href="https://astranoto.com/"
+						text="@astranoto"
+					/>
+				</div>
+				<div class="w-[200px] md:w-[150px] lg:w-[200px]">
+					<p class="font-bold tracking-tight">student</p>
+					<GlitchText
+						class="cursor-pointer tracking-tighter"
+						text="engineering@unibo"
+					/>
+				</div>
 			</div>
-			<ThemeSwitcher />
+			<div class="order-1 flex justify-between md:order-3 md:justify-evenly">
+				<div class="w-[200px] md:w-[180px] lg:w-[200px]">
+					<p class="font-bold tracking-tight">Luca Corsetti</p>
+					<GlitchText
+						class="cursor-pointer tracking-tighter"
+						text="fullstack developer"
+					/>
+				</div>
+				<ThemeSwitcher />
+			</div>
 		</div>
-	</navbar>
+	</nav>
 </template>
 
 <script lang="ts" setup>
-import GlitchText from './ui/GlitchText.vue';
 import ThemeSwitcher from './ThemeSwitcher.vue';
+import GlitchText from './ui/GlitchText.vue';
 import MemojiLoop from './ui/MemojiLoop.vue';
 </script>
 
