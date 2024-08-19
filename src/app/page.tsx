7378c90fe9116f1f0bc8e8bc3834688acd978367
ilcors-dev <corsetti.luca28@gmail.com>
2024-08-19 08:59:29 +0200

    new content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,22 +20,31 @@ export default function Home() {
     <main className="container mx-auto p-4 sm:p-10 lg:p-6">
       <Navbar />
       <div className="mt-20 space-y-8">
-        <h1 className="text-2xl font-bold sm:text-4xl">
+        <h1 className="text-center text-2xl font-bold sm:text-start sm:text-4xl">
           👋🏻 I&apos;m{" "}
           <span className="bg-gradient-to-r from-purple-500 to-purple-300 bg-clip-text text-transparent dark:from-green-500 dark:to-green-300">
             Luca
           </span>
           , a software engineer from Italy, Bologna.
         </h1>
-        <p className="text-lg sm:text-xl">
+        <p className="text-center text-lg sm:text-start sm:text-xl">
           👨🏻‍💻 I talk to computers and they (sometimes) respond to me.
         </p>
-        <p className="text-lg sm:text-xl">
+        <p className="text-center text-lg sm:text-start sm:text-xl">
           📚 I hold a BSc degree in computer engineering and I&apos;m currently
           pursuing a Master&apos;s degree in the same field.
         </p>
+        <p className="text-center text-lg sm:text-start sm:text-xl">
+          🔬 I&apos;m a curious person. In my free time, I love experimenting
+          with new technologies, learning new things, and trying new programming
+          languages.
+        </p>
+        <p className="text-center text-lg sm:text-start sm:text-xl">
+          ⛰️ I&apos;m not afraid of challenges; they are meant to be overcome. I
+          enjoy feeding my brain with new problems to solve.
+        </p>
       </div>
-      <div className="mt-8 flex items-center gap-4">
+      <div className="mt-8 flex items-center justify-center gap-4 sm:justify-start">
         <Link href="https://github.com/ilcors-dev">
           <Github size={24} />
         </Link>
