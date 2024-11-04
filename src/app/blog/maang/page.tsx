b365d5481eb8b7c7e0aa5495f7ad0f1294e9252a
ilcors-dev <corsetti.luca28@gmail.com>
2024-11-04 22:35:17 +0100

    minor

--- src/app/blog/maang/page.tsx
+++ src/app/blog/maang/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "My journey to MAANG",
+  title: "My journey to a MAANG",
   description:
     "The path that led me into a MAANG. From the beginning until the first achievement.",
 };
